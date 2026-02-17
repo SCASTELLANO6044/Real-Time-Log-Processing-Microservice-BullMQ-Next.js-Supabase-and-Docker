cba019b2bd9dce9ee533a08997279c64941d9daa
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-02-17 21:43:04 +0100

    :sparkles: Refactor replace express js project by NextJS project

--- backend/db/supabase.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(process.env.SUPABASE_LINK, process.env.SUPABASE_API_KEY)
-
-module.exports = supabase;
